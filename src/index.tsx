a7d6462e5e75fdaf9d0007f50162869006bdde04
Gabriel <gabeguimaraes13@gmail.com>
2022-06-29 10:09:17 -0300

    Ajusta index

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,13 @@
-import { ThemeProvider } from "@mui/material";
 import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./App";
-import { theme } from "config/theme";
+import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
 
